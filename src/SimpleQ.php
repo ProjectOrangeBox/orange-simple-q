2f20ae2e20417bd261421753ee433d9cf05fbfeb
Don Myers <donmyers@me.com>
2020-07-21 22:30:40 -0400

    updates

--- src/SimpleQ.php
+++ src/SimpleQ.php
@@ -10,7 +10,7 @@ use simpleq\Exceptions\SimpleQException;
 class SimpleQ extends CI_Model
 {
 	protected $table = 'simple_q';
-	protected $status_map = ['new'=>10,'tagged'=>20,'processed'=>30,'error'=>40];
+	protected $status_map = ['new' => 10, 'tagged' => 20, 'processed' => 30, 'error' => 40];
 	protected $status_map_flipped;
 	protected $db;
 	protected $clean_up_hours;
@@ -24,7 +24,7 @@ class SimpleQ extends CI_Model
 	{
 		$this->status_map_flipped = array_flip($this->status_map);
 
-		$config = ci('config')->merged('simple_q',[
+		$config = \configMerge('simple_q', [
 			'tablename' => $this->table,
 			'clean up hours' => 168 /* 7 days */,
 			'requeue tagged hours' => 1 /* 1 hour */,
@@ -32,7 +32,7 @@ class SimpleQ extends CI_Model
 			'token length' => 40 /* sha1 length */,
 			'database group' => 'default',
 			'garbage collection percent' => 50 /* percent */
-		],$config);
+		], $config);
 
 		$this->clean_up_hours = $config['clean up hours'];
 		$this->retag_hours = $config['requeue tagged hours'];
@@ -63,29 +63,29 @@ class SimpleQ extends CI_Model
 		return md5($this->queue);
 	}
 
-	public function push($data,string $queue = null): bool
+	public function push($data, string $queue = null): bool
 	{
 		if ($queue !== null) {
 			$this->queue($queue);
 		}
 
-		return $this->db->insert($this->table, ['created'=>date('Y-m-d H:i:s'),'status'=>$this->status_map['new'],'payload'=>$this->encode($data),'queue'=>$this->get_queue(),'token'=>null]);
+		return $this->db->insert($this->table, ['created' => date('Y-m-d H:i:s'), 'status' => $this->status_map['new'], 'payload' => $this->encode($data), 'queue' => $this->get_queue(), 'token' => null]);
 	}
 
-	public function pull($queue=null) /* record or false if nothing found */
+	public function pull($queue = null) /* record or false if nothing found */
 	{
 		$token = hash($this->token_hash, uniqid('', true));
 
-		$this->db->set(['token'=>$token,'status'=>$this->status_map['tagged'],'updated'=>date('Y-m-d H:i:s')])->where(['status'=>$this->status_map['new'],'token is null'=>null,'queue'=>$this->get_queue($queue)])->limit(1)->update($this->table);
+		$this->db->set(['token' => $token, 'status' => $this->status_map['tagged'], 'updated' => date('Y-m-d H:i:s')])->where(['status' => $this->status_map['new'], 'token is null' => null, 'queue' => $this->get_queue($queue)])->limit(1)->update($this->table);
 
-		if ($success = (bool)$this->db->affected_rows()) {
-			$record = $this->db->limit(1)->where(['token'=>$token])->get($this->table)->row();
+		if ($success = (bool) $this->db->affected_rows()) {
+			$record = $this->db->limit(1)->where(['token' => $token])->get($this->table)->row();
 
 			$record->status_raw = $record->status;
 			$record->status = $this->status_map_flipped[$record->status];
 			$record->payload = $this->decode($record);
 
-			$success = new SimpleQrecord($record,$this);
+			$success = new SimpleQrecord($record, $this);
 		}
 
 		return $success;
@@ -94,11 +94,11 @@ class SimpleQ extends CI_Model
 	public function cleanup(): SimpleQ
 	{
 		if ($this->retag_hours > 0) {
-			$this->db->set(['token'=>null,'status'=>$this->status_map['new'],'updated'=>date('Y-m-d H:i:s')])->where(['updated < now() - interval '.(int)$this->retag_hours.' hour'=>null,'status'=>$this->status_map['tagged']])->update($this->table);
+			$this->db->set(['token' => null, 'status' => $this->status_map['new'], 'updated' => date('Y-m-d H:i:s')])->where(['updated < now() - interval ' . (int) $this->retag_hours . ' hour' => null, 'status' => $this->status_map['tagged']])->update($this->table);
 		}
 
 		if ($this->clean_up_hours > 0) {
-			$this->db->where(['updated < now() - interval '.(int)$this->clean_up_hours.' hour'=>null,'status'=>$this->status_map['processed']])->delete($this->table);
+			$this->db->where(['updated < now() - interval ' . (int) $this->clean_up_hours . ' hour' => null, 'status' => $this->status_map['processed']])->delete($this->table);
 		}
 
 		return $this;
@@ -108,15 +108,15 @@ class SimpleQ extends CI_Model
 	public function update($token, $status): bool
 	{
 		if (!array_key_exists($status, $this->status_map)) {
-			throw new SimpleQException('Unknown Simple Q record status "'.$status.'".');
+			throw new SimpleQException('Unknown Simple Q record status "' . $status . '".');
 		}
 
-		return $this->db->limit(1)->update($this->table, ['token'=>null,'updated'=>date('Y-m-d H:i:s'),'status'=>$this->status_map[$status]], ['token'=>$token]);
+		return $this->db->limit(1)->update($this->table, ['token' => null, 'updated' => date('Y-m-d H:i:s'), 'status' => $this->status_map[$status]], ['token' => $token]);
 	}
 
 	public function complete(string $token): bool
 	{
-		return $this->update($token,'processed');
+		return $this->update($token, 'processed');
 	}
 
 	public function new(string $token): bool
@@ -158,13 +158,13 @@ class SimpleQ extends CI_Model
 		switch ($payload_record->type) {
 			case 'object':
 				$data = $payload_record->data;
-			break;
+				break;
 			case 'array':
-				$data = (array)$payload_record->data;
-			break;
+				$data = (array) $payload_record->data;
+				break;
 			case 'scalar':
 				$data = $payload_record->data;
-			break;
+				break;
 			default:
 				throw new SimpleQException('Could not determine Simple Q data type.');
 		}
@@ -185,5 +185,4 @@ class SimpleQ extends CI_Model
 	{
 		return ($this->create_checksum($payload) == $checksum);
 	}
-
 } /* end class */
